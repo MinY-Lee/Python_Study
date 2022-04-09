3b764fc545b5278e04b732cbe2c67109a3bfb18e
Minyoung-Lee <leemy08@naver.com>
2022-04-09 21:20:03 +0900

    구현 - 치킨 배달 풀이  RE

--- 구현/치킨_배달.py
+++ 구현/치킨_배달.py
@@ -3,7 +3,7 @@ from itertools import combinations  # 조합 라이브러리 - 전체 N개 중 M
 
 # 도시 크기, 치킨집 수 입력받기
 # 이건 생각했음
-n, m = map(int(input().split()))
+n, m = map(int, input().split())
 
 chicken, house = [], []
 
@@ -26,5 +26,21 @@ def get_sum(candidate):
     for hx, hy in house:
         # 가장 가까운 치킨집을 찾기
         temp = 1e9  # e는 10의 거듭제곱을 나타냄 -> 1 X 10의 9승
+        print(temp)
         for cx, cy in candidate:
-            temp = min(temp, abs(hx - cx) + abs(hy - cy))
+            # 치킨집 까지의 거리
+            temp = min(temp, abs(hx - cx) + abs(hy - cy))   # abs() 함수: 절댓값
+            # 왜 temp가 1e9인가 궁금했는데 최솟값을 찾기 위함이어서 그럼
+            # 13Cm의 값의 최대가 100,000이므로 이것 이상이면 올바른 값이 나올 듯.
+            # 밑의 result도 비슷한 의미같음.
+        # 가장 가까운 치킨집까지의 거리를 더하기
+        result += temp
+    # 치킨 거리의 합 반환
+    return result
+
+# 치킨 거리의 합의 최소를 찾아 출력
+result = 1e9
+for candidate in candidates:
+    result = min(result, get_sum(candidate))
+    
+print(result)
