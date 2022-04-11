a90dbc092f9f50e6005239df9976e971069e2b5d
Minyoung-Lee <leemy08@naver.com>
2022-04-11 23:50:42 +0900

    구현 - 외벽 점검 풀이(코드분석하기)

--- 구현/외벽_점검.py
+++ 구현/외벽_점검.py
@@ -17,4 +17,16 @@ def solution(n, weak, dist):
         for friends in list(permutations(dist, len(dist))):
             count = 1   # 투입할 친구의 수
             # 해당 친구가 점검할 수 있는 마지막 위치
-            position = weak[start] + friends[count - 1]     # 여기가 왜인지 이해가 안감 좀 더 공부해서 마저 완성하기!
+            position = weak[start] + friends[count - 1]
+            # 시작점부터 모든 취약 지점 확인
+            for index in range(start, start + length):
+                # 점검할 수 있는 위치를 벗어난 경우
+                if position < weak[index]:
+                    count += 1  # 새로운 친구 투입
+                    if count > len(dist):   # 더 투입 불가능하면 종료
+                        break
+                    position = weak[index] + friends[count - 1]
+            answer = min(answer, count) # 최솟값 계산
+    if answer > len(dist):
+        return -1
+    return answer
