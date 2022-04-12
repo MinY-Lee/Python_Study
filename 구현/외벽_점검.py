dfeb68aa2b04dbe7d43614397eed58eaeed3c66c
Minyoung-Lee <leemy08@naver.com>
2022-04-12 22:50:33 +0900

    구현 - 외벽 점검 RE

--- 구현/외벽_점검.py
+++ 구현/외벽_점검.py
@@ -7,7 +7,7 @@ from itertools import permutations
 
 def solution(n, weak, dist):
     # 길이를 2배로 늘려서 '원형'을 일자 형태로 변형
-    length = len(weak)
+    length = len(weak)  # ?? 길이가 2배인데 왜 이렇게 해 줘야 하는지??
     for i in range(length):
         weak.append(weak[i] + n)    # weak의 마지막에 추가해주기
     answer = len(dist) + 1  # 투압할 친구 수의 최솟값을 찾아야 하므로 len(dist) + 1로 초기화
@@ -21,7 +21,7 @@ def solution(n, weak, dist):
             # 시작점부터 모든 취약 지점 확인
             for index in range(start, start + length):
                 # 점검할 수 있는 위치를 벗어난 경우
-                if position < weak[index]:
+                if position < weak[index]:  # 포지션보다 적어야 거리가 부족해 인원이 투입되니까
                     count += 1  # 새로운 친구 투입
                     if count > len(dist):   # 더 투입 불가능하면 종료
                         break
