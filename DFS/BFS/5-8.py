587edbee0377bcbe59f1be083941890950d86845
Minyoung-Lee <leemy08@naver.com>
2022-04-18 22:44:50 +0900

    DFS/BFS - 5-8 DFS예제

--- DFS/BFS/5-8.py
+++ DFS/BFS/5-8.py
@@ -2,7 +2,7 @@
 def dfs(graph, v, visited):
     # 현재 노드를 방문 처리
     visited[v] = True
-    print(v, end='')
+    print(v, end =' ')
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
         if not visited[i]:
@@ -20,3 +20,9 @@ graph = [
     [2, 6, 8],
     [1, 7]
 ]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 DFS 함수 호출
+dfs(graph, 1, visited)
