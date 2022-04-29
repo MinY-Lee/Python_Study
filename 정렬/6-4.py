7bee1c8ae22d540b6ab9aae411122160b2c82d95
Minyoung-Lee <leemy08@naver.com>
2022-04-29 23:33:19 +0900

    정렬 - 퀵 정렬 소스코드 미완

--- 정렬/6-4.py
+++ 정렬/6-4.py
@@ -4,3 +4,12 @@ def quick_sort(array, start, end):
     if start >= end:    # 원소가 1개인 경우 종료
         return
     pivot = start   # 피벗은  첫 번째 원소
+    left = start + 1
+    right = end
+    while left <= right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
