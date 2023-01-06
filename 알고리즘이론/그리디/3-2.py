ca28204054d3b27131e5981fc2f408b3f7ee1786
MinY-Lee <leemy08@naver.com>
2023-01-06 23:39:46 +0900

    실전문제 2 큰 수의 법칙 예시답안 추가

--- 알고리즘이론/그리디/3-2.py
+++ 알고리즘이론/그리디/3-2.py
@@ -23,4 +23,14 @@ while True:     # 총 m번 돌아가기
     m -= 1
 # True일 동안은 계속 돌아가기 때문에 두번째 큰 수 한번 더해주고 다시 제일 큰 수를 k번 더해주는 식으로 간다
 
+# # 또 다른 방법의 경우(답안예시)
+# count = int(m / (k + 1)) * k
+# count += m % (k + 1)
+
+# result = 0
+# result += (count) * first # 가장 큰 수 더하기
+# result += (m - count) * second # 두 번째로 큰 수 더하기
+
+# print(result)   # 최종 답안 출력
+
 print(result)
